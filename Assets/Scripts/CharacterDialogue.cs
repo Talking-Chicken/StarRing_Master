f7d9d568c4dbb45d769fc0e5c73a366a6a8485db
Leo Ji <sj3026@nyu.edu>
2023-02-26 11:55:27 -0500

    imported naughty attribute

---
+++ Assets/Scripts/CharacterDialogue.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+using MoreMountains.TopDownEngine;
+
+public class CharacterDialogue : CharacterAbility
+{
+    public override string HelpBoxText() { return "This component allows your character to trigger dialogue when pressing left mouse button.";}
+}
