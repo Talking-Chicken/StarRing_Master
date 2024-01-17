a9aae1e14261cb0d008d5a5bdce4d92329154278
David (Wenxuan) Liao <98340743+RabbitSociety@users.noreply.github.com>
2024-01-16 20:36:02 -0800

    yarn file update

--- Assets/YarnCommandManager.cs
+++ Assets/YarnCommandManager.cs
@@ -46,5 +46,10 @@ public class YarnCommandManager : MonoBehaviour
     {
         feedback.PlayFeedbacks(); 
     }
+    [YarnCommand("stopInteraction")]
+    public void stopInteraction()
+    {
+     Interactable.StopInteract();
+    }
 
 }
